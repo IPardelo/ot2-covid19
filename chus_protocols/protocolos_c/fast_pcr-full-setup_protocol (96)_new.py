6891d4141e6bc18293577b00a68ca7b2c7298c5f
Ismael Castiñeira Paz <info@osmeusproxectos.es>
2020-11-02 12:49:47 +0100

    First interface version - by Victoria Suárez Ulloa

---
+++ chus_protocols/protocolos_c/fast_pcr-full-setup_protocol (96)_new.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+
+import math
+import importlib
+from opentrons import protocol_api
+from opentrons.types import Point
+import json
+
+# Load library
+LIBRARY_PATH = '/root/ot2-covid19/library/'
+#LIBRARY_PATH = '/home/laboratorio/Repositorios/ot2-covid19/library/'
+
+spec = importlib.util.spec_from_file_location("library.protocols.common_functions",
+                                              "{}protocols/common_functions.py".format(LIBRARY_PATH))
+common = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(common)
+
+# Load Brands & other stuff
+spec2 = importlib.util.spec_from_file_location("library.protocols.lab_stuff",
+                                              "{}protocols/lab_stuff.py".format(LIBRARY_PATH))
+lab_stuff = importlib.util.module_from_spec(spec2)
+spec2.loader.exec_module(lab_stuff)
+
+# Load specific run parameters from run_config.json created by custom GUI
+#filepath = "/root/ot2-covid19/library/protocols/run_config.json"
+filepath = "{}protocols/run_config.json".format(LIBRARY_PATH)
+with open(filepath) as f:
+    cont = json.load(f)
+
+
+# ------------------------
+# Protocol parameters
+# ------------------------
+numero_muestras = int(cont["muestras"])                         # Número de muestras (sin las muestras de control)
+                                            # (máximo 94 si es una mastermix, o 44 si es doble)                                            
+brand_name = cont["kit"]
+tipo_de_tubo = cont["tubo"]                  # Tipo de tubo que contiene el ARN: 'labturbo' o 'criotubo'
+
+
+
+metadata = {
+    'protocolName': 'C1',
+    'author': 'Luis Lorenzo Mosquera, Victor Soñora Pombo & Ismael Castiñeira Paz',
+    'source': 'Hospital Clínico Universitario de Santiago (CHUS)',
+    'apiLevel': '2.0',
+    'description': 'PCR preparation, mix the master mix and rna sample in pcr plate'
+}
+
+
+def fast_dispense(pipette, reagent, source, dests, vol_aspirate, vol_dispense, air_gap_vol, x_offset, pickup_height, disp_height,
+                          blow_out, touch_tip):
+    current_pipete_volume = 0
+    dests = list(reversed(dests))
+    while len(dests) > 0:
+        if current_pipete_volume < vol_dispense:
+            vol_aspirate = (len(dests) * vol_dispense) if (len(dests) * vol_dispense) <= vol_aspirate else vol_aspirate
+            # Source
+            s = source.bottom(pickup_height).move(Point(x=x_offset[0]))
+            pipette.aspirate(vol_aspirate, s, rate=1.2)
+            # If there is air_gap_vol, switch pipette to slow speed
+            if air_gap_vol != 0:
+                pipette.aspirate(air_gap_vol, source.top(z=-2), rate=reagent.get('flow_rate_aspirate'))
+            # Apply a delay, if there is any
+            delay = reagent.get('delay')
+            if delay:
+                ctx.delay(seconds=delay)
+            current_pipete_volume = vol_aspirate
+        # Go to destination
+        dest = dests.pop()
+        drop = dest.top(z=disp_height).move(Point(x=x_offset[1]))
+        pipette.dispense(vol_dispense, drop, rate=reagent.get('flow_rate_dispense'))
+        current_pipete_volume -= vol_dispense
+    pipette.drop_tip()
+
+
+# ------------------------
+# Other parameters
+# ------------------------
+_, _, _, _, pickup_height = lab_stuff.tubes(tipo_de_tubo)
+master_mix_vol, arn_vol, doble_mix = lab_stuff.brands(brand_name)
+num_cols = math.ceil(numero_muestras / 8)
+x_offset = [0, 0]
+air_gap_vol_source = 2
+diameter_sample = 8.25
+area_section_sample = (math.pi * diameter_sample**2) / 4
+
+
+# following volumes in ul
+master_mix = {
+    'name': 'master mix',
+    'flow_rate_aspirate': 1,
+    'flow_rate_dispense': 1,
+    'rinse': False,
+    'delay': 0,
+    'reagent_reservoir_volume': 1500,
+    'num_wells': 1,
+    'h_cono': 4,
+    'v_cono': 4 * area_section_sample * diameter_sample * 0.5 / 3,
+    'vol_well_original': 1500,
+    'vol_well': 1500,
+    'unused': [],
+    'col': 0,
+    'vol_well': 0
+}
+
+rna_sample = {
+    'name': 'RNA samples',
+    'flow_rate_aspirate': 1,
+    'flow_rate_dispense': 1,
+    'rinse': False,
+    'delay': 0,
+    'reagent_reservoir_volume': 200 * 24,
+    'num_wells': 24,
+    'h_cono': 4,
+    'v_cono': 4 * area_section_sample * diameter_sample * 0.5 / 3,
+    'vol_well_original': 200,
+    'vol_well': 200,
+    'unused': [],
+    'col': 0,
+    'vol_well': 0
+}
+
+
+# ----------------------------
+# Main
+# ----------------------------
+def run(ctx: protocol_api.ProtocolContext):
+    # ------------------------
+    # Load LabWare
+    # ------------------------
+    # Tip racks
+    tips_20 = [ctx.load_labware('opentrons_96_filtertiprack_20ul', slot, '20µl filter tiprack') for slot in ['10']]
+    tips_200 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', slot, '200µl filter tiprack') for slot in ['11']]
+
+    # Pipettes
+    p20 = ctx.load_instrument('p20_single_gen2', 'right', tip_racks=tips_20)
+    p300 = ctx.load_instrument('p300_single_gen2', 'left', tip_racks=tips_200)
+
+    # Source (master_mix in and deep-weel with NUM SAMPLES x RNA samples)
+#    source_master_mix = ctx.load_labware('opentrons_24_aluminumblock_generic_2ml_screwcap', '7', 'Bloque Aluminio opentrons 24 screwcaps 2000 µL')
+    source_master_mix = ctx.load_labware('opentrons_24_aluminumblock_nest_1.5ml_snapcap', '7', 'Bloque Aluminio opentrons 24 eppendorf 1500 µL')
+    source_master_mix = source_master_mix.wells()
+    
+    MAX_NUM_OF_SOURCES = 96
+    MIN_NUM_OF_SOURCES = 4
+    NUM_OF_SOURCES_PER_RACK = 24
+    rack_num = math.ceil(numero_muestras / NUM_OF_SOURCES_PER_RACK) if numero_muestras < MAX_NUM_OF_SOURCES else MIN_NUM_OF_SOURCES
+    source_rna_samples = common.generate_source_table([ctx.load_labware(
+#        'opentrons_24_tuberack_generic_2ml_screwcap', slot,
+        'opentrons_24_tuberack_nest_1.5ml_snapcap', slot,
+#        'source tuberack with screwcap' + str(i + 1)) for i, slot in enumerate(['5', '6', '2', '3'][:rack_num])
+        'source tuberack with snapcap' + str(i + 1)) for i, slot in enumerate(['5', '6', '2', '3'][:rack_num])
+    ])
+    sources_rna = source_rna_samples[:numero_muestras]
+
+
+    # Destination (NUM SAMPLES x pcr plate)
+    pcr_plate_destination = ctx.load_labware('abi_fast_qpcr_96_alum_opentrons_100ul', '1', 'chilled qPCR final plate')
+    destinations = pcr_plate_destination.wells()
+
+    mastermix_mov = [
+        (source_master_mix[0], pcr_plate_destination.wells()[:numero_muestras + 2])
+    ]
+
+    if doble_mix:
+        mastermix_mov += [(source_master_mix[1], pcr_plate_destination.wells()[48:48 + numero_muestras + 2])]
+
+    # ------------------
+    # Protocol
+    # ------------------
+    # Dispense master mix
+    for s, d in mastermix_mov:
+        if not p300.hw_pipette['has_tip']:
+            common.pick_up(p300)
+        vol_aspirate = (numero_muestras * master_mix_vol) if (numero_muestras * master_mix_vol) <= (10 * master_mix_vol) else (10 * master_mix_vol)
+        fast_dispense(p300, reagent=rna_sample, source=s, dests=d,
+                                         vol_aspirate=vol_aspirate, vol_dispense=master_mix_vol, air_gap_vol=air_gap_vol_source,
+                                         x_offset=x_offset, pickup_height=pickup_height,
+                                         disp_height=-10, blow_out=True, touch_tip=True)    
+
+    # Dispense RNA
+    for i in range(0, numero_muestras):
+        if not p20.hw_pipette['has_tip']:
+            common.pick_up(p20)
+
+        source = sources_rna[i]
+        destination = destinations[i]
+        common.move_vol_multichannel(ctx, p20, reagent=rna_sample, source=source, dest=destination,
+                                         vol=arn_vol, air_gap_vol=air_gap_vol_source,
+                                         x_offset=x_offset, pickup_height=pickup_height,
+                                         disp_height=-10, blow_out=True, touch_tip=True)
+        p20.drop_tip()
+
+        if doble_mix:
+            destination = destinations[48 + i]
+            common.pick_up(p20)
+            common.move_vol_multichannel(ctx, p20, reagent=rna_sample, source=source, dest=destination,
+                                         vol=arn_vol, air_gap_vol=air_gap_vol_source,
+                                         x_offset=x_offset, pickup_height=pickup_height,
+                                         disp_height=-10, blow_out=True, touch_tip=True)
+            p20.drop_tip()
+
+        
---
+++ chus_protocols/protocolos_c/fast_pcr96_app.py
@@ -0,0 +1,121 @@
+#!/home/laboratorio/anaconda2/bin/python
+
+
+from gooey import Gooey, GooeyParser
+import subprocess
+import datetime
+
+
+
+robodict = {'SCR1':'69.101.94.152', 
+	    'SCR2':'69.101.94.151'}
+splitmethods = ['genomica', 
+		'vircell']
+LIBRARY_PATH = '/home/laboratorio/Repositorios/ot2-covid19/library/'
+
+
+### Gooey decorator
+
+@Gooey(program_name='Opentrons: configuracion',
+       default_size=(500, 700),
+       show_stop_warning = True,
+       force_stop_is_error = True,
+       show_success_modal = False,
+       show_failure_modal = False,
+       show_restart_button = False,
+       language = 'spanish',
+       image_dir = '/home/laboratorio/Imagenes/')
+#       navigation='TABBED')
+
+
+def parse_args():
+
+    parser = GooeyParser()
+
+    robots = parser.add_argument("robots",
+            metavar='ROBOT',
+            action='store',
+            choices=['SCR1', 'SCR2'],
+            default='SCR1')
+
+# text field to input number of samples
+    samples = parser.add_argument("samples", 
+            metavar='MUESTRAS',
+            type=int,
+            help="Inserte el numero de muestras", 
+            action="store")
+
+# menus for type of library kit and type of tube
+    prot_group = parser.add_argument_group('Protocolo fast 96')    
+    prot_group.add_argument("modes",
+            metavar='KIT DE REACTIVOS',
+            action = 'store',
+            choices=['seegene-2019-ncov', 'seegene-sars-cov2', 'thermofisher', 'roche', 'vircell', 'vircell_multiplex', 'genomica'],
+            default='genomica')
+    prot_group.add_argument("tubes",
+            metavar='TIPO DE TUBO',
+            action = 'store',
+            choices=['falcon', 'eppendorf', 'labturbo','criotubo', 'criotubo_conico', 'serologia', 'f_redondo', 'f_redondo2'],
+            default='eppendorf')
+
+    args=parser.parse_args()
+    return args
+
+
+def print_output():
+
+    numero_muestras_text = str(numero_muestras)
+
+    now = datetime.datetime.now()
+
+    print(now.strftime('%d-%m-%Y %H-%M\n\n'))
+    print('\tSE HAN SELECCIONADO '+ numero_muestras_text +' MUESTRAS\n')
+
+    if brand_name in splitmethods and numero_muestras > 47:
+        raise ValueError("### PARA ESTE METODO EL NUMERO MAXIMO DE MUESTRAS ES 47 ###\n\n")
+    elif 94 < numero_muestras:
+        raise ValueError("#### EL NUMERO MAXIMO DE MUESTRAS ES 94 ###\n\n")
+
+    print('\tEL KIT '+brand_name+'\n')
+    print('\tY EL TUBO '+tipo_de_tubo+'\n\n\n')
+
+
+def print_json():
+
+
+    with open(filepath, 'w') as f:
+        f.write('{\n\t\"muestras\": '+numero_muestras_text+',\n\t\"kit\": \"'+brand_name+'\",\n\t\"tubo\": \"'+tipo_de_tubo+'\"\n}')
+
+
+def send():
+
+    subprocess.call(["scp", "-i", "/home/laboratorio/ot2_ssh_key_sinclave", filepath, "root@"+ip+":/root/ot2-covid19/library/protocols/"])
+
+
+
+
+if __name__ == '__main__':
+
+# variables
+    args = parse_args()
+
+    robot = args.robots
+    ip = robodict[robot]
+
+    numero_muestras = args.samples
+    numero_muestras_text = str(numero_muestras)
+
+    brand_name = args.modes
+
+    tipo_de_tubo = args.tubes
+
+
+
+    filepath = "{}protocols/run_config.json".format(LIBRARY_PATH)
+
+#ejecucion
+    print_output()
+
+    print_json()
+
+    send()
